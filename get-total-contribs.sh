a82c898afad1c73c6eb9de284f9508ae88f1d955
IMOitself <russjonn@gmail.com>
2025-06-27 01:43:56 +0800

    feat: finish script that gets total contributions
    refactor: function now inputs variables as well

--- get-total-contribs.sh
+++ get-total-contribs.sh
@@ -5,29 +5,55 @@ GITHUB_TOKEN=$(sed -n '2p' sensitive-info.txt)
 
 get_json_from_graphql() {
   local graphql=$1
-
+  local graphql_variables=$2
   local formatted_graphql=$(echo "$graphql" | tr -d '\n' | sed 's/  */ /g')
-
-  local query='{"query": "'$formatted_graphql'", "variables": {"username": "'$GITHUB_USERNAME'"}}'
-
+  local query='{"query": "'$formatted_graphql'", "variables": '$graphql_variables'}'
   local json=$(curl -s -H "Authorization: bearer $GITHUB_TOKEN" -X POST -d "$query" https://api.github.com/graphql)
-
   echo "$json"
 }
 
-get_years_graphql='
+
+
+
+
+get_creation_date_graphql='
 query($username: String!) {
   user(login: $username) {
-    contributionsCollection {
-      contributionYears
+    createdAt
+  }
+}'
+
+json=$(get_json_from_graphql "$get_creation_date_graphql" '{"username": "'$GITHUB_USERNAME'"}')
+creation_date=$(echo "$json" | jq -r '.data.user.createdAt')
+creation_year=$(echo "$creation_date" | cut -d '-' -f 1)
+current_year=$(date +'%Y')
+
+years=$(seq $creation_year $current_year)
+
+
+
+
+
+total_contribs=0
+
+for year in $years; do
+  from_date="${year}-01-01T00:00:00Z"
+  to_date="${year}-12-31T23:59:59Z"
+
+  get_contribs_graphql='
+query($username: String!, $from_date: DateTime!, $to_date: DateTime!) {
+  user(login: $username) {
+    contributionsCollection(from: $from_date, to: $to_date) {
+      contributionCalendar {
+        totalContributions
+      }
     }
   }
 }'
 
-json=$(get_json_from_graphql "$get_years_graphql")
-
-years=$(echo "$json" | jq -r '.data.user.contributionsCollection.contributionYears[]')
-
-for year in $years; do
-  echo $year
+  json=$(get_json_from_graphql "$get_contribs_graphql" '{"username": "'$GITHUB_USERNAME'", "from_date": "'$from_date'", "to_date": "'$to_date'"}')
+  contribs=$(echo "$json" | jq -r '.data.user.contributionsCollection.contributionCalendar.totalContributions')
+  total_contribs=$(($total_contribs + $contribs))
 done
+
+echo $total_contribs
