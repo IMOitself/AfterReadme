c3c67d6eb14dc87c990e9a5cfb145c1a18ba975f
IMOitself <russjonn@gmail.com>
2025-06-26 16:23:23 +0800

    feat: add script to fetch github user info via graphql
    Added get-streak.sh to query GitHub user details using credentials from sensitive-info.txt.
    Updated .gitignore to exclude sensitive-info.txt.

---
+++ get-streak.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+GITHUB_USERNAME=$(sed -n '1p' sensitive-info.txt)
+GITHUB_TOKEN=$(sed -n '2p' sensitive-info.txt)
+
+graphql='
+query($username: String!) {
+  user(login: $username) {
+    name
+    login
+    bio
+  }
+}'
+
+formatted_graphql=$(echo "$graphql" | tr -d '\n' | sed 's/  */ /g')
+
+query='{"query": "'$formatted_graphql'", "variables": {"username": "'$GITHUB_USERNAME'"}}'
+
+query_json=$(curl -s -H "Authorization: bearer $GITHUB_TOKEN" -X POST -d "$query" https://api.github.com/graphql)
+
+echo $query_json
+
+# { "message": "Problems parsing JSON", "documentation_url": "https://docs.github.com/graphql", "status": "400" }
