86d6797dfc8d04dccf831a20ac0cfdecf1a3275f
IMOitself <russjonn@gmail.com>
2025-06-30 14:19:42 +0800

    feat: test update readme by emoji
    Adds a formatted GitHub stats section with placeholders for contributions and streak, then replaces those placeholders with actual values using sed. This improves the script's output formatting for easier integration into README files.

--- readme-updater.sh
+++ readme-updater.sh
@@ -6,4 +6,16 @@ echo "$total_contribs"
 daily_streak=$(bash "get-daily-streak.sh")
 echo "$daily_streak"
 
+test="
+## GitHub Stats
 
+❤️ contribs: 0
+🔥 streak: 0
+"
+
+echo "$test"
+
+test=$(echo "$test" | sed -E 's/.*❤️([^:]+): .*/❤️\1: '$total_contribs'/g')
+test=$(echo "$test" | sed -E 's/.*🔥([^:]+): .*/🔥\1: '$daily_streak'/g')
+
+echo "$test"
